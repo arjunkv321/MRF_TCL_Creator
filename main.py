69aff29e684faa2151f254b4c65fed27fe17fdfa
unknown <arjunkv321@gmail.com>
2021-06-16 19:02:01 +0530

    functions added to main and some modification upto panelzone

--- main.py
+++ main.py
@@ -8,7 +8,23 @@ HStoryTyp = 13.12
 extColDepth = [24.74,24.48,24.31,23.92] #exterior column depth, 2 successive colunms have same depth
 intColDepth = [25,25,24.48,23.92] #interior column depth, 2 successive colunms have same depth
 beamDepth = [26.92,24.31,23.92,17.99] #beam column depth, 2 successive colunms have same depth
+FloorWeight=586.25
 
 with open('test.tcl','w') as Element:
-	elemPanelZone2DCreator(NStory,NBay,Element)
-    
+	fileDetails(NStory,NBay,Element)
+with open('test.tcl','a') as Element:
+	defineBuildingGeometry(NStory,NBay,WBay,HStory1,HStoryTyp,Element)
+	locationsOfColumns(NBay,Element)
+	locationsOfBeams(NStory,Element)
+	panelZoneDimExt(NStory,extColDepth,Element)
+	panelZoneDimInt(NStory,intColDepth,Element)
+	panelZoneDimVert(NStory,beamDepth,Element)
+	plasticHingeOffset(NStory,Element)
+	CalculateNodalMass(NBay,FloorWeight,Element)
+	defineNodes(NStory,NBay,Element)
+	ColumnHingeNodes(NStory,NBay,Element)
+	beamHingeNodes(NStory,NBay,Element)
+	panelZoneNodes(NStory,NBay,Element)
+	nodalmasscreator(NStory,NBay,Element)
+	degreesOfFreedom(NStory,NBay,Element)
+	assignBoundaryCondidtions(NBay,Element)
--- sample.py
+++ sample.py
@@ -23,30 +23,38 @@ def defineBuildingGeometry(story,bay,WBays,HStory1,Hstorytyp,Element):
 
 
 def nodalmasscreator(story,bay,Element):
+    print("""# define nodal masses:  lump at beam-column joints in frame
+	# command: mass $nodeID5$dof1mass $dof2mass $dof3mass""",file=Element)
     for floor in range(2,story+2):
-        for pier in range(1,bay+1):
-            nodalmass="mass {p}{f}05 $NodalMass $Negligible $Negligible;	# Pier {p}, Floor {f}".format(p=pier,f=floor)
+        for pier in range(1,bay+2):
+            nodalmass="    mass {p}{f}05 $NodalMass $Negligible $Negligible;	# Pier {p}, Floor {f}".format(p=pier,f=floor)
             print(nodalmass,file=Element)
         print("",file=Element)
 
 def degreesOfFreedom(story,bay,Element):
+    print("""# constrain beam-column joints in a floor to have the same lateral displacement using the "equalDOF" command
+	# command: equalDOF $MasterNodeID $SlaveNodeID $dof1 $dof2...""",file=Element)
     for floor in range(2,story+2):
             for pier in range(2,bay+3):
                 if pier<=bay+1:
-                    dof="equalDOF 1{f}05 {p}205 $dof1;		# Floor {f}:  Pier 1 to Pier {p}".format(p=pier,f=floor)
+                    dof="    equalDOF 1{f}05 {p}205 $dof1;		# Floor {f}:  Pier 1 to Pier {p}".format(p=pier,f=floor)
                     print(dof,file=Element)
                 else:
-                    dof="equalDOF 1{f}05 {p}{f} $dof1;		        # Floor {f}:  Pier 1 to Pier {p}".format(p=pier,f=floor)
+                    dof="    equalDOF 1{f}05 {p}{f} $dof1;		    # Floor {f}:  Pier 1 to Pier {p}".format(p=pier,f=floor)
                     print(dof,file=Element)
             print("",file=Element)
 
 def assignBoundaryCondidtions(bay,Element):
+    print("""# assign boundary condidtions 
+	# command:  fix nodeID dxFixity dyFixity rzFixity
+	# fixity values: 1 = constrained; 0 = unconstrained
+	# fix the base of the building; pin P-delta column at base""",file=Element)
     for pier in range(1,bay+3):
         if pier<bay+2:
-            boundarycondition="""fix {p}1 1 1 {d};""".format(p=pier,d=str(1))
+            boundarycondition="""    fix {p}1 1 1 {d};""".format(p=pier,d=str(1))
             print (boundarycondition,file=Element)  
         else:
-            boundarycondition="""fix {p}1 1 1 {d};""".format(p=pier,d=str(0))
+            boundarycondition="""    fix {p}1 1 1 {d};   # P-delta column is pinned""".format(p=pier,d=str(0))
             print (boundarycondition,file=Element)    
 
 def locationsOfColumns(NBay,Element):
@@ -60,16 +68,16 @@ def locationsOfColumns(NBay,Element):
         print("\n   ",columns,end="",file=Element)
     print(" # P-delta column line",file=Element)
 
-def locationsOfBeams(NStory):
+def locationsOfBeams(NStory,Element):
     beam12="""
     set Floor1 0.0;		# ground floor
     set Floor2 [expr $Floor1 + $HStory1];"""
-    print("\t",beam12)
+    print("\t",beam12,file=Element)
     for i in range(3,NStory+2):
         beams="set Floor{f} [expr $Floor{downf} + $HStoryTyp];".format(
             f=i, downf=i-1 
         )
-        print("   ",beams)
+        print("   ",beams,file=Element)
 
 def elementTrussPdelta(story,bay,Element): 
     for floor in range(2,story+2):
@@ -134,7 +142,7 @@ def defineNodes(NStory,NBays,Element):
     for i in range(1,NBays+3):
         node = "node {b}1 $Pier{b} $Floor1;".format(b=i)
         print("   ",node,file=Element)
-    for i in range(2,NStory+1):
+    for i in range(2,NStory+2):
         node = "node 5{s} $Pier5 $Floor{s};".format(s=i)
         print("   ",node,file=Element)
 
@@ -228,35 +236,39 @@ def Ks_beam(NStory,Nbays,Element):
             print("   ",ksb,file=Element)
 
 def ColumnHingeNodes(NStory,NBay,Element):
-	vert=2
-	for NStory in range(1,NStory+1):
-		for dir in range(2):
-			if dir%2==0:
-				if NStory>1:
-					print("    # column hinges at bottom of story {s}".format(s=NStory),file=Element)
-					for pier in range(1,NBay+3):
-						if pier < NBay+2:
-							print(("    node {p}{f}7 $Pier{p} [expr $Floor{f} + $phvert{v1}{v2}];").format(p=pier,f=NStory,v1=vert,v2=vert+1),file=Element)
-							print(("    node {p}{f}8 $Pier{p} [expr $Floor{f} + $phvert{v1}{v2}];").format(p=pier,f=NStory,v1=vert,v2=vert+1),file=Element)
-						else:
-							print(("    node {p}{f}7 $Pier{p} $Floor{f};	# zero-stiffness spring will be used on p-delta column").format(p=pier,f=NStory),file=Element)
-					if NStory%2==1:
-						print("    # column hinges at mid of story {s}".format(s=NStory),file=Element)
-						for pier in range(1,NBay+2):
-							print(("    node {p}{s}0 $Pier1 [expr $Floor{s} + $phvert{v1}{v2} + 0.5*$HNStoryTyp];	#xy0, x=pier y=floor").format(p=pier,s=NStory,v1=vert,v2=vert+1),file=Element)
-						vert+=2
-				else:
-					print( "    # column hinges at bottom of story 1 (base)",file=Element)
-					for pier in range(1,NBay+2):
-						print(("    node {p}17 $Pier{p} $Floor1;").format(p=pier),file=Element)
-			else:
-				print("    # column hinges at top of story {s}".format(s=NStory),file=Element)
-				for pier in range(1,NBay+3):
-					if pier < NBay+2:
-						print(("    node {p}{f}5 $Pier{p} [expr $Floor{f} - $phvert{v1}{v2}];").format(p=pier,f=NStory+1,v1=vert,v2=vert+1),file=Element)
-						print(("    node {p}{f}6 $Pier{p} [expr $Floor{f} - $phvert{v1}{v2}];").format(p=pier,f=NStory+1,v1=vert,v2=vert+1),file=Element)
-					else:
-						print(("    node {p}{f}6 $Pier{p} $Floor{f};	# zero-stiffness spring will be used on p-delta column\n").format(p=pier,f=NStory+1,f2=NStory),file=Element)
+    print("""\n# define extra nodes for plastic hinge rotational springs
+	# nodeID convention:  "xya" where x = Pier #, y = Floor #, a = location relative to beam-column joint
+	# "a" convention: 1,2 = left; 3,4 = right; (used for beams)
+	# "a" convention: 5,6 = below; 7,8 = above; (used for columns)""",file=Element)
+    vert=2
+    for NStory in range(1,NStory+1):
+        for dir in range(2):
+            if dir%2==0:
+                if NStory>1:
+                    print("    # column hinges at bottom of story {s}".format(s=NStory),file=Element)
+                    for pier in range(1,NBay+3):
+                        if pier < NBay+2:
+                            print(("    node {p}{f}7 $Pier{p} [expr $Floor{f} + $phvert{v1}{v2}];").format(p=pier,f=NStory,v1=vert,v2=vert+1),file=Element)
+                            print(("    node {p}{f}8 $Pier{p} [expr $Floor{f} + $phvert{v1}{v2}];").format(p=pier,f=NStory,v1=vert,v2=vert+1),file=Element)
+                        else:
+                            print(("    node {p}{f}7 $Pier{p} $Floor{f};	# zero-stiffness spring will be used on p-delta column").format(p=pier,f=NStory),file=Element)
+                    if NStory%2==1:
+                        print("    # column hinges at mid of story {s}".format(s=NStory),file=Element)
+                        for pier in range(1,NBay+2):
+                            print(("    node {p}{s}0 $Pier1 [expr $Floor{s} + $phvert{v1}{v2} + 0.5*$HNStoryTyp];	#xy0, x=pier y=floor").format(p=pier,s=NStory,v1=vert,v2=vert+1),file=Element)
+                        vert+=2
+                else:
+                    print( "    # column hinges at bottom of story 1 (base)",file=Element)
+                    for pier in range(1,NBay+2):
+                        print(("    node {p}17 $Pier{p} $Floor1;").format(p=pier),file=Element)
+            else:
+                print("    # column hinges at top of story {s}".format(s=NStory),file=Element)
+                for pier in range(1,NBay+3):
+                    if pier < NBay+2:
+                        print(("    node {p}{f}5 $Pier{p} [expr $Floor{f} - $phvert{v1}{v2}];").format(p=pier,f=NStory+1,v1=vert,v2=vert+1),file=Element)
+                        print(("    node {p}{f}6 $Pier{p} [expr $Floor{f} - $phvert{v1}{v2}];").format(p=pier,f=NStory+1,v1=vert,v2=vert+1),file=Element)
+                    else:
+                        print(("    node {p}{f}6 $Pier{p} $Floor{f};	# zero-stiffness spring will be used on p-delta column\n").format(p=pier,f=NStory+1,f2=NStory),file=Element)
 
 def elasticColumnElement(NStory,Nbay,Element):
     header="""# set up geometric transformation of elements
@@ -323,7 +335,7 @@ def beamHingeNodes(story,bay,Element):
 
 
 def panelZoneNodes(story,bay,Element):
-    header=""""# define extra nodes for panel zones
+    header="""# define extra nodes for panel zones
     # nodeID convention:  "xybc" where x = Pier #, y = Floor #, bc = location relative to beam-column joint
     # "bc" conventions: 01,02 = top left of joint; 
     # 					03,04 = top right of joint;
@@ -357,6 +369,8 @@ def panelZoneNodes(story,bay,Element):
     node {p}{f}09 [expr $Pier{p} - $pzlatext{f1}{f2} ] [expr $Floor{f} - $phvert{f1}{f2}];
     node {p}{f}10 [expr $Pier{p} - $pzlatext{f1}{f2} ] [expr $Floor{f}];""".format(f=floor,p=pier,f1=temp,f2=temp+1)
                 print(panelzoneNode,file=Element)
+                if floor==story+1:
+                    print(f"    node {pier}{floor}7  [expr $Pier{pier}]  [expr $Floor{floor} + $phvert{temp}{temp+1}]; # not previously defined since no column above",file=Element)
                 print("",file=Element)
             else:
                 panelzoneNode="""# panel zone at Pier {p}, Floor {f}
@@ -371,4 +385,65 @@ def panelZoneNodes(story,bay,Element):
     node {p}{f}09 [expr $Pier{p} - $pzlatint{f1}{f2} ] [expr $Floor{f} - $phvert{f1}{f2}];
     node {p}{f}10 [expr $Pier{p} - $pzlatint{f1}{f2} ] [expr $Floor{f}];""".format(f=floor,p=pier,f1=temp,f2=temp+1)
                 print(panelzoneNode,file=Element)
+                if floor==story+1:
+                    print(f"    node {pier}{floor}7  [expr $Pier{pier}]  [expr $Floor{floor} + $phvert{temp}{temp+1}]; # not previously defined since no column above",file=Element)
                 print("",file=Element)
+
+def fileDetails(NStory,NBay,Element):
+    print(("""# --------------------------------------------------------------------------------------------------
+# Example: {s}-Story {b}-Bay Steel Moment Frame with Concentrated Plasticity
+# Panel Zone Model with Concentrated Plastic Hinges at RBS Locations
+# Created by:  Laura Eads, Stanford University, 2010
+# Units: kips, inches, seconds
+
+# Element ID conventions:
+#	1xy    = frame columns with RBS springs at both ends
+#	2xy    = frame beams with RBS springs at both ends
+#	6xy    = trusses linking frame and P-delta column
+#	7xy    = P-delta columns
+#	2xya   = frame beams between panel zone and RBS spring
+#	3xya   = frame column rotational springs
+#	4xya   = frame beam rotational springs
+#	5xya   = P-delta column rotational springs
+#	4xy00  = panel zone rotational springs
+#	500xya = panel zone elements
+#	where:
+#		x = Pier or Bay #
+#		y = Floor or Story #
+#		a = an integer describing the location relative to beam-column joint (see description where elements and nodes are defined)
+
+###################################################################################################
+#          Set Up & Source Definition									  
+###################################################################################################
+	wipe all;							# clear memory of past model definitions
+	model BasicBuilder -ndm 2 -ndf 3;	# Define the model builder, ndm = #dimension, ndf = #dofs
+	source DisplayModel2D.tcl;			# procedure for displaying a 2D perspective of model
+	source DisplayPlane.tcl;			# procedure for displaying a plane in a model
+	source rotSpring2DModIKModel.tcl;	# procedure for defining a rotational spring (zero-length element) for plastic hinges
+	source rotLeaningCol.tcl;			# procedure for defining a rotational spring (zero-length element) with very small stiffness
+	source rotPanelZone2D.tcl;			# procedure for defining a rotational spring (zero-length element) to capture panel zone shear distortions
+	source elemPanelZone2D.tcl;			# procedure for defining 8 elements to create a rectangular panel zone
+	
+###################################################################################################
+#          Define Analysis Type										  
+###################################################################################################
+# Define type of analysis:  "pushover" = pushover;  "dynamic" = dynamic
+	set analysisType "pushover";
+	
+	if {l}$analysisType == "pushover"{r} {l}
+		set dataDir Concentrated-PanelZone-Pushover-Output;	# name of output folder
+		file mkdir $dataDir; 								# create output folder
+	{r}
+	if {l}$analysisType == "dynamic"{r} {l}
+		set dataDir Concentrated-PanelZone-Dynamic-Output;	# name of output folder
+		file mkdir $dataDir; 								# create output folder
+	{r}
+	""").format(s=NStory,b=NBay,l='{',r='}'),file=Element)
+
+def CalculateNodalMass(NBay,FloorWeight,Element):
+    print(f"""# calculate nodal masses -- lump floor masses at frame nodes
+	set g 386.2;				# acceleration due to gravity
+	set FloorWeight {FloorWeight};		# weight of all Floor  in kips
+	set WBuilding  [expr $FloorWeight * $NStories]; # total building weight
+	set NodalMass [expr ($FloorWeight/$g) / ({float(NBay+1)})];	# mass at each node on Floors
+	set Negligible 1e-9;	# a very small number to avoid problems with zero""",file=Element)
